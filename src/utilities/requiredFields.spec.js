c77d492eed5abab8779ed48875eacab943fb5064
oshalygin <oshalygin@gmail.com>
2017-01-02 23:13:24 -0800

    Add tests to requiredFields

--- src/utilities/requiredFields.spec.js
+++ src/utilities/requiredFields.spec.js
@@ -34,4 +34,45 @@ describe('Required Fields', () => {
     expect(actual).deep.equals(expected);
   });
 
+  it('should return false for the email property if it was not set', () => {
+    const {email, ...updatedArgv} = argv; //eslint-disable-line no-unused-vars
+
+    const expected = {
+      email: false,
+      registry: true,
+      secureToken: true
+    };
+
+    const actual = parseRequiredProperties(updatedArgv);
+    expect(actual).deep.equals(expected);
+  });
+
+  it('should return false for the registry property if it was not set', () => {
+    const {registry, ...updatedArgv} = argv; //eslint-disable-line no-unused-vars
+
+    const expected = {
+      email: true,
+      registry: false,
+      secureToken: true
+    };
+
+    const actual = parseRequiredProperties(updatedArgv);
+    expect(actual).deep.equals(expected);
+  });
+
+  it('should return false for the secureToken property if it was not set', () => {
+    const updatedArgv = { ...argv };
+    delete updatedArgv['secure-token'];
+
+    const expected = {
+      email: true,
+      registry: true,
+      secureToken: false
+    };
+
+    const actual = parseRequiredProperties(updatedArgv);
+    expect(actual).deep.equals(expected);
+  });
+
+
 });
